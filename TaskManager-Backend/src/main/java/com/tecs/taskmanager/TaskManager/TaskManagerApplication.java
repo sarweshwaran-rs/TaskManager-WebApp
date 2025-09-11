9c4c4568aaccab6becc246951b5856c79cd986e6
= <sarweshchandran@gmail.com>
2025-09-11 07:26:29 +0530

    V1.0 Added the Main Application

---
+++ TaskManager-Backend/src/main/java/com/tecs/taskmanager/TaskManager/TaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.tecs.taskmanager.TaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+}
