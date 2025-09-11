f0897ba3114094d2daac2b67544a89860b6775e2
= <sarweshchandran@gmail.com>
2025-09-11 12:22:24 +0530

    V1.0 Added the SystemController which handles the RestAPI for os, processes, memory, disk, gpu

---
+++ TaskManager-Backend/src/main/java/com/tecs/taskmanager/TaskManager/controller/SystemController.java
@@ -0,0 +1,52 @@
+package com.tecs.taskmanager.TaskManager.controller;
+
+import com.tecs.taskmanager.TaskManager.service.SystemMonitorService;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api")
+public class SystemController {
+    private final SystemMonitorService service;
+
+    public SystemController(SystemMonitorService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/cpu")
+    public Map<String, Object> getCpuInfo() {
+        return service.getCpuInfo();
+    }
+
+    @GetMapping("/memory")
+    public Map<String, Object> getMemoryInfo() {
+        return service.getMemoryInfo();
+    }
+    
+    @GetMapping("/os")
+    public Map<String, Object> getOsInfo() {
+        return service.getOsInfo();
+    }    
+
+    @GetMapping("/processes")
+    public List<Map<String, Object>> getProcesses(@RequestParam(defaultValue = "10") int limit) {
+        return service.getProcesses(limit);
+    }
+
+    @GetMapping("/gpu")
+    public List<Map<String, Object>> getGpuInfo() {
+        return service.getGpuInfo();
+    }
+
+    @GetMapping("/disk")
+    public List<Map<String, Object>> getDiskInfo() {
+        return service.getDiskInfo();
+    }
+
+    @GetMapping("/network")
+    public List<Map<String, Object>> getNetworkInfo() {
+        return service.getNetworkInfo();
+    }
+}
