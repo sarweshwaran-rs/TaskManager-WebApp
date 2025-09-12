834f8db2a5fa1d65b5703f7b74021443bbcc3c05
= <sarweshchandran@gmail.com>
2025-09-12 17:03:54 +0530

    V1.0 Added the Logger information to the WebSocketConfig

--- TaskManager-Backend/src/main/java/com/tecs/taskmanager/TaskManager/config/WebsocketConfig.java
+++ TaskManager-Backend/src/main/java/com/tecs/taskmanager/TaskManager/config/WebsocketConfig.java
@@ -1,5 +1,8 @@
 package com.tecs.taskmanager.TaskManager.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,14 +13,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
  class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+     private static final Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
+
+    public WebSocketConfig() {
+        logger.info("WebsocketConfig loaded and configured.");
+    }
+    
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        // This is the prefix for topics that clients can subscribe to from the server.
-        config.enableSimpleBroker("/api");
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic","/api");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         // This registers the "/ws" endpoint, allowing clients to connect.
         // setAllowedOriginPatterns("*") allows connections from any origin, which is perfect for local development.
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
