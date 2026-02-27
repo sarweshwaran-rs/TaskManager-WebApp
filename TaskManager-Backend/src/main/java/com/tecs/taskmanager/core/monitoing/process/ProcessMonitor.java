e72ef095775aee161c1ec30a712af7d08fdfa0ee
sarweshwaran-rs <sarweshchandran@gmail.com>
2026-02-27 17:14:07 +0530

    feat: Extend ProcessMonitor interface with additional methods for process retrieval

--- TaskManager-Backend/src/main/java/com/tecs/taskmanager/core/monitoing/process/ProcessMonitor.java
+++ TaskManager-Backend/src/main/java/com/tecs/taskmanager/core/monitoing/process/ProcessMonitor.java
@@ -3,7 +3,12 @@ package com.tecs.taskmanager.core.monitoing.process;
 import java.util.List;
 
 import com.tecs.taskmanager.dto.process.ProcessInfoDTO;
+import com.tecs.taskmanager.dto.process.ProcessTreeDTO;
 
 public interface ProcessMonitor {
     List<ProcessInfoDTO> getTopProcesses(int limit);
+    List<ProcessInfoDTO> getAllProcesses();
+    List<ProcessTreeDTO> getProcessTree();
+    ProcessTreeDTO getProcessByPid(int pid);
+    List<ProcessInfoDTO> getProcessByName(String name);
 }
